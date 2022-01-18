5284c78774aa2d9cfa06482b5290f99dd9ad9549
zohre99 <47241298+zohre99@users.noreply.github.com>
2022-01-18 22:11:27 +0330

    first-commit

---
+++ src/main/java/com/zohre/jenkinstutorials/JenkinsTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.zohre.jenkinstutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTutorialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zohre/jenkinstutorials/JenkinsTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zohre.jenkinstutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
